af1988ed8a089847c8e0ac890963c7903044799b
yangyboy <yangyboy@163.com>
2020-06-05 09:48:21 +0800

    Initial commit

---
+++ src/main/java/com/function/functionlearning/FunctionLearningApplication.java
@@ -0,0 +1,13 @@
+package com.function.functionlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunctionLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/function/functionlearning/FunctionLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.function.functionlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
